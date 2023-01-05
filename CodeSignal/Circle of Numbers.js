5842f9fc0235ff16ab9e3bbebf508a8c029994da
bryanparmelee <bryan.parmelee@gmail.com>
2023-01-05 16:03:48 -0500

    Add solution

---
+++ CodeSignal/Circle of Numbers.js
@@ -0,0 +1,15 @@
+//https://app.codesignal.com/arcade/intro/level-7/vExYvcGnFsEYSt8nQ
+
+function solution(n, firstNumber) {
+    let circle = [];
+    while (n > 0) {
+        circle.unshift(n - 1);
+        n--;
+    }
+    const halfway = circle.length / 2;
+    const start = circle.indexOf(firstNumber);
+    const firstSlice = circle.slice(start);
+    const secondSlice = circle.slice(0, start)
+    const updatedCircle = [...firstSlice, ...secondSlice];
+    return updatedCircle[halfway];
+}
