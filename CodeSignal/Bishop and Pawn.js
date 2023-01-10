4352a783b45b62bd0132c01fd936ec3a9eb378c2
bryanparmelee <bryan.parmelee@gmail.com>
2023-01-10 17:32:00 -0500

    Reformatting

--- CodeSignal/Bishop and Pawn.js
+++ CodeSignal/Bishop and Pawn.js
@@ -2,6 +2,6 @@
 
 function solution(bishop, pawn) {
     const a = Math.abs(bishop.charCodeAt(0) - pawn.charCodeAt(0));
-        const b = Math.abs(Number(bishop[1]) - Number(pawn[1]));
-        return a === b
+    const b = Math.abs(Number(bishop[1]) - Number(pawn[1]));
+    return a === b;
 }
