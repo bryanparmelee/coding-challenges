aae3de420765a663a6a6ef0f8111e2a25ea2fbf1
bryanparmelee <bryan.parmelee@gmail.com>
2023-01-14 11:31:50 -0500

    Refactor solution

--- CodeSignal/chessKnight.js
+++ CodeSignal/chessKnight.js
@@ -3,21 +3,22 @@
 function solution(cell) {
     const x = cell.charCodeAt(0) - 96;
     const y = Number(cell[1]); 
-    console.log(x, y);
-    let moves = [];
-    moves.push([x+2, y+1]);
-    moves.push([x+2, y-1]);
-    moves.push([x-2, y+1]);
-    moves.push([x-2, y-1]);
-    moves.push([x+1, y+2]);
-    moves.push([x+1, y-2]);
-    moves.push([x-1, y+2]);
-    moves.push([x-1, y-2]);
+    let moves = [
+        [2, 1],
+        [2, -1],
+        [-2, 1],
+        [-2, -1],
+        [1, 2],
+        [1, -2],
+        [-1, 2],
+        [-1, -2]
+    ];
+ 
     const filtered = moves.filter(el => {
-        return el[0] > 0 && 
-               el[1] > 0 &&
-               el[0] < 9 &&
-               el[1] < 9;
+        return x + el[0] > 0 && 
+               y + el[1] > 0 &&
+               x + el[0] < 9 &&
+               y + el[1] < 9;
     })
     return filtered.length; 
 }
