51834bbbb0f7de0ef27754af4b89c7541e18a85f
bryanparmelee <bryan.parmelee@gmail.com>
2023-01-16 16:37:18 -0500

    Refactor solution

--- CodeSignal/digitsProduct.js
+++ CodeSignal/digitsProduct.js
@@ -1,14 +1,14 @@
 //https://app.codesignal.com/arcade/intro/level-12/NJJhENpgheFRQbPRA
 
 function solution(product) {
-    for (let i = 1; i < 5000; i++) {
-        if (products(i) === product)return i;
-    }
-        function products (num) {
-            return String(num)
-                .split('')
-                .map(Number)
-                .reduce((a, b) => a*b, 1);
+    if (product === 0) return 10;
+    if (product === 1) return 1;
+    let output = [];
+    for (let i = 9; i > 1; i--) {
+        while (product % i === 0) {
+            output.unshift(i);
+            product /= i;
         }
-        return -1;
+    }
+    return product > 1 ? -1 : Number(output.join(''));
 }
