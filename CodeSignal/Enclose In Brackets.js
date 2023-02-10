53b65576d60b22ec975daddfe3be62a7eac4c502
bryanparmelee <bryan.parmelee@gmail.com>
2023-02-10 14:38:36 -0500

    Refactor solution

--- CodeSignal/Enclose In Brackets.js
+++ CodeSignal/Enclose In Brackets.js
@@ -1,9 +1,5 @@
 //https://app.codesignal.com/arcade/code-arcade/book-market/K2ieDm98sPDzzMepz
 
 function solution(inputString) {
-
-    let arr = inputString.split('');
-    arr.push(')');
-    arr.unshift('(');
-    return arr.join('');
+    return `(${inputString})`;
 }
