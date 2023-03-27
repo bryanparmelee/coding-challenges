a6bba79b93fd15f3a594122bf2bd6b94aea1d3f6
bryanparmelee <bryan.parmelee@gmail.com>
2023-03-26 22:17:34 -0400

    Add solution

---
+++ CodeSignal/The Core/14. Sorting Outpost/Maximum Sum.js
@@ -0,0 +1,19 @@
+//https://app.codesignal.com/arcade/code-arcade/sorting-outpost/64koZSDqndwYxFZj6
+
+function solution(a, q) {
+  a.sort((x, y) => x - y);
+  let counts = new Array(a.length).fill(0);
+
+  for (let tuple of q) {
+    for (let i = tuple[0]; i <= tuple[1]; i++) {
+      counts[i]++;
+    }
+  }
+  counts.sort((x, y) => x - y);
+
+  return a.reduce((sum, x, i) => {
+    return sum + x * counts[i];
+  }, 0);
+}
+
+console.log(solution());
