cc866c3a334fee2412027abe4ad5671452d76e18
bryanparmelee <bryan.parmelee@gmail.com>
2023-04-02 21:53:47 -0400

    Add solution

--- CodeSignal/The Core/15. Chess Tavern/Chess Bishop Dream.js
+++ CodeSignal/The Core/15. Chess Tavern/Chess Bishop Dream.js
@@ -1,29 +1,11 @@
 //https://app.codesignal.com/arcade/code-arcade/chess-tavern/FfjPkYvo9ah6wgXem
 
 function solution(boardSize, initPosition, initDirection, k) {
-  const h = boardSize[0] - 1;
-  const w = boardSize[1] - 1;
-  let [y, x] = initPosition;
-  let [a, b] = initDirection;
-  let yK = k % (boardSize[0] * 2);
-  let xK = k % (boardSize[1] * 2);
-  while (yK > 0) {
-    if (y === 0 || y === h) {
-      a *= -1;
-      yK = yk - 2;
-    } else {
-      y += a;
-      yK--;
-    }
-  }
-  while (xK > 0) {
-    if (x === 0 || x === w) {
-      b *= -1;
-      xK = xK - 2;
-    } else {
-      x += b;
-      xK--;
-    }
-  }
-  return [y, x];
+  return [0, 1].map((i) => {
+    const mod = boardSize[i] * 2;
+    const mid = (mod - 1) / 2;
+    const mad = initPosition[i] + initDirection[i] * k;
+
+    return mid - Math.abs(mid - (((mad % mod) + mod) % mod));
+  });
 }
