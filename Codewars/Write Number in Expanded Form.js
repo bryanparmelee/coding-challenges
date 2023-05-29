d326e943a5999487f2c4855b6244702d5c231f0d
bryanparmelee <bryan.parmelee@gmail.com>
2023-05-29 16:55:21 -0400

    Solved

---
+++ Codewars/Write Number in Expanded Form.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/5842df8ccbd22792a4000245/javascript
+
+function expandedForm(num) {
+  let nArr = num.toString().split("");
+  nArr = nArr.map((n, i, a) => {
+    if (n !== "0") {
+      return n.padEnd(a.length - i, "0");
+    }
+    return n;
+  });
+  return nArr.filter((n) => n !== "0").join(" + ");
+}
+
+console.log(expandedForm(70304));
