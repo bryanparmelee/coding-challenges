8b8d569491508d4a7365d8907cf2e71d51fc8225
bryanparmelee <bryan.parmelee@gmail.com>
2023-06-02 14:17:26 -0400

    Solved

---
+++ Codewars/A Needle in the Haystack.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf("needle")}`;
+}
