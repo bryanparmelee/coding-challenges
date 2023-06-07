2a9710545a7d84bbb88025a0ab55843582d8bed6
bryanparmelee <bryan.parmelee@gmail.com>
2023-06-07 14:46:47 -0400

    Solved

---
+++ Codewars/Basic Mathematical Operations.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+
+function basicOp(operation, value1, value2) {
+  if (operation === "+") return value1 + value2;
+  if (operation === "-") return value1 - value2;
+  if (operation === "*") return value1 * value2;
+  if (operation === "/") return value1 / value2;
+}
