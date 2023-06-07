f7e1bc87ee34ef3d061853a23bacf60d7578b4aa
bryanparmelee <bryan.parmelee@gmail.com>
2023-06-06 21:19:55 -0400

    A=Solved

---
+++ Codewars/You're a square.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+
+var isSquare = function (n) {
+  return Math.floor(Math.sqrt(n)) === Math.sqrt(n);
+};
+
+function isSquare2(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
