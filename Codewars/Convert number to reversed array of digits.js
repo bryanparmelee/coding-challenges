c568142b64c1045ca0c007b96ddcd0faa4d88f29
bryanparmelee <bryan.parmelee@gmail.com>
2023-06-25 20:53:40 -0400

    Solved

---
+++ Codewars/Convert number to reversed array of digits.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+
+function digitize(n) {
+  return n.toString().split("").map(Number).reverse();
+}
---
+++ Codewars/Is a number prime.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/5262119038c0985a5b00029f/train/javascript
+
+function isPrime(num) {
+  if (num < 2) return false;
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; ++i) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
