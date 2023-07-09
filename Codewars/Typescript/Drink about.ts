0a90e945de8eb35364842fbe1174bfa1d53b6de6
bryanparmelee <bryan.parmelee@gmail.com>
2023-07-08 21:02:05 -0400

    Solved

---
+++ Codewars/Typescript/Drink about.ts
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/56170e844da7c6f647000063/train/typescript
+
+export function peopleWithAgeDrink(old: number): string {
+  if (old < 14) {
+    return "drink toddy";
+  } else if (old >= 14 && old < 18) {
+    return "drink coke";
+  } else if (old >= 18 && old < 21) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+}
